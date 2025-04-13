9f7eec6dcf4bc6f8b7f814a288935a07a0afcc3a
yuuzy <adcb20090916@163.com>
2025-04-13 17:07:20 +0800

    0.0.2

--- readme.md
+++ readme.md
@@ -3,3 +3,17 @@
 [![npm](https://img.shields.io/npm/v/koishi-plugin-loop-execute-cmd?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-loop-execute-cmd)
 
 循环执行指令多次
+
+# 更新日志
+
+## v0.0.2
+
+- 更多的防小人
+- 完善**package.json**
+- 在**readme.md**中加入更新日志
+- 加入配置项`maxTime`
+
+## v0.0.1
+
+- 完成基础功能
+- 完成基础的防小人
--- src/index.tsx
+++ src/index.tsx
@@ -3,23 +3,39 @@ import {} from '@satorijs/element'
 
 export const name = 'loop-execute-cmd'
 
-export const dependency = ['@satorijs/element']
+export interface Config {
+  maxTime: number
+}
 
-export interface Config {}
+export const Config: Schema<Config> = Schema.object({
+  maxTime: Schema.number().default(10).description('最大执行次数').min(5).max(25),
+})
 
-export const Config: Schema<Config> = Schema.object({})
-
-export const usage = '## 这里看着好空，写点东西在这^^'
-
-export function apply(ctx: Context) {
-  ctx.command('for <time: number> <cmd: text>', '循环执行命令')
+export function apply(ctx: Context, cfg: Config) {
+  const logger = ctx.logger('loop-execute-cmd')
+  ctx.command('for <time: number> <cmd: text>', '循环执行命令，若为小数则向下取整')
+    .alias('loop')
     .example('for 5 echo hello 输出5次 hello')
     .action(async ({ session }, time, cmd) => {
       const timeNum = Number(time)
+
       if (isNaN(timeNum) || timeNum <= 0) {
         await session.send('执行次数必须是大于0的数字！')
         return
       }
+
+      const regex1 = /for*/
+      const regex2 = /loop*/
+      if (regex1.test(cmd) || regex2.test(cmd)) {
+        await session.send('命令中不能包含for或loop关键字！')
+        return
+      }
+
+      if (timeNum > cfg.maxTime) {
+        await session.send(`执行次数不能大于${cfg.maxTime}！`)
+        return
+      }
+
       const t = Math.floor(timeNum)
       for (let i = 0; i < t; i++) {
         await session.send(<execute>{cmd}</execute>)
